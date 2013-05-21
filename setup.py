edaab60dbc675ac6957d6378099b3b4acae111a2
SAPikachu <sapikachu@gmail.com>
2013-05-21 20:35:17 +0800

    Setup script

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
+with open("requirements.txt", "r") as f:
+    requirements = [l.strip() for l in f if l.strip()]
+
+setup(
+    name="igstools",
+    version="0.9",
+    description="Tools for parsing bluray IGS menus",
+    long_description=long_description,
+    author="Joe Hu (SAPikachu)",
+    author_email="i@sapika.ch",
+    url="https://github.com/SAPikachu/igstools",
+    packages=["igstools"],
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": ["igstools = igstools.__main__:main"],
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Multimedia :: Video",
+    ],
+)
